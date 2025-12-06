84192daa42838a87c2cd49f86f16d9ff31c5f647
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-12-06 21:50:00 +0100

    solution day 1

--- 2025/day01/day1.py
+++ 2025/day01/day1.py
@@ -8,7 +8,6 @@ def read_puzzle(file):
 
 def main():
     puzzle = read_puzzle("day1.txt")
-    # puzzle = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"]
 
     part1 = 0
     part2 = 0
