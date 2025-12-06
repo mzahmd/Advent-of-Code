a2c27570b237d2aed2c9157ffff266e113d2ea46
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-12-06 21:59:11 +0100

    solution for day 2

--- 2025/day02/day2.py
+++ 2025/day02/day2.py
@@ -14,20 +14,19 @@ def main():
 
         for number in range(start, end+1):
             number_as_string = str(number)
-            length_of_number = int(len(number_as_string))
+            length_of_number = len(number_as_string)
             length_of_number_div_by_2 = length_of_number // 2
 
             if length_of_number % 2 == 0:
                 number1 = number_as_string[0:length_of_number_div_by_2]
                 number2 = number_as_string[length_of_number_div_by_2:]
-                part1 += int(number) if int(number1) == int(number2) else 0
+                part1 += int(number) if number1 == number2 else 0
                 
             for l in range(1, length_of_number_div_by_2 + 1):
                 if number_as_string == number_as_string[:l] * (len(number_as_string) // l):
                     part2 += number
                     break
                 
-    # (17077011375)
     print((part1, part2))
 
 if __name__ == '__main__':
